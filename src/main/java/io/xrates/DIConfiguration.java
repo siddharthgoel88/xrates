51be72c6d6836015df7714c16158cf42edd35768
Harshul <gandhi.harshul.88@gmail.com>
2015-07-21 23:43:52 +0800

    Dependency injection added for DBS Rate Provider Implementation - fixed Issue #4

---
+++ src/main/java/io/xrates/DIConfiguration.java
@@ -0,0 +1,17 @@
+package io.xrates;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import io.xrates.MainScheduler;
+import io.xrates.DBSRateProviderImpl;
+
+@Configuration
+@ComponentScan(value={"io.xrates"})
+public class DIConfiguration {
+	
+	@Bean
+	public DBSRateProviderImpl getRateProviderType(){
+		return new DBSRateProviderImpl();
+	}
+}
--- src/main/java/io/xrates/MainScheduler.java
+++ src/main/java/io/xrates/MainScheduler.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -12,12 +13,19 @@ import org.springframework.stereotype.Component;
 public class MainScheduler {
 	
 	Logger log = LoggerFactory.getLogger(MainScheduler.class.getName());
-	@Scheduled(fixedRate = 5000)
+	IRateProvider dbsrpObj;
+	
+	@Autowired
+	public void setRateProvider(DBSRateProviderImpl dbsRateProvider){
+		this.dbsrpObj = dbsRateProvider;
+	}
+	
+	@Scheduled(fixedRate = 4000)
 	public void getRate() throws IOException {
 		log.debug("Running scheduler");
-		
-		IRateProvider dbsrpObj = new DBSRateProviderImpl();
-		log.info(String.valueOf(dbsrpObj.convert(Currency.SGD, Currency.INR)));
+//		IRateProvider dbsrpObj = new DBSRateProviderImpl();
+		log.info("For 1 "+Currency.SGD+" you get "+String.valueOf(this.dbsrpObj.convert(Currency.SGD, Currency.INR))+" "+Currency.INR);
 	}
-
+	
+	
 }
