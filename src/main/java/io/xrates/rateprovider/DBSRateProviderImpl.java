d115eaa833cce4b2761df23162247a7ba3e1a251
Siddharth Goel <siddharth98391@gmail.com>
2015-08-08 16:37:34 +0800

    Issue #2: Refactored DBS implementation
    Need to follow the same pattern for other implementations also
    
    Signed-off-by: Siddharth Goel <siddharth98391@gmail.com>

--- src/main/java/io/xrates/rateprovider/DBSRateProviderImpl.java
+++ src/main/java/io/xrates/rateprovider/DBSRateProviderImpl.java
@@ -16,68 +16,59 @@ import org.slf4j.LoggerFactory;
 
 import io.xrates.Rates;
 import io.xrates.constants.RateProvider;
+import io.xrates.rateprovider.util.CurrencyAdapter;
 
 public class DBSRateProviderImpl extends AbstractRateProvider {
-	private double toINR=0.0;
-	private double toSGD=0.0;
 	private String resourceURL = "http://www.dbs.com.sg/personal/rates-online/foreign-currency-foreign-exchange.page";
 	private Logger log = LoggerFactory.getLogger(DBSRateProviderImpl.class.getName());
+	private Currency base = Currency.getInstance("SGD");
 	private Rates rates = getRates();
-	private String from_curr = "Indian Rupee";
 	
 	public DBSRateProviderImpl() {
 		setRateProvider(RateProvider.DBS);
+		rates.addAvailableCurrency(base);
 	}
 	
 	@Override
 	protected void updateRates() {
 		log.debug("Inside updateRates of DBSRateProviderImpl");
-		updateListOfCurrencies();
 		try {
-			fetchRates();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	private void fetchRates() throws IOException {
 			log.info("DBS: Fetching page @ " + resourceURL);
 			Document doc = Jsoup.connect(resourceURL).get();
-			Element rateTableDiv = doc.select("div.rates-table").get(1);
-			Element rateTable = rateTableDiv.select("table tbody").get(1);
-			getRateForInputCurr(from_curr, rateTable);
+			extractRates(doc.select("div.rates-table").get(0).select("table tbody").get(0));
+			extractRates(doc.select("div.rates-table").get(1).select("table tbody").get(1));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
 	}
 	
-	private void getRateForInputCurr(String from_curr, Element rateTable){
-		int length = rateTable.select("tr").size();	//will be used to loop through to parse entire table
-		double targetCurr = 0.0;
-		double equivalentsgd = 0.0;
-		for (int i = 0;i<length;i++){
-			Element currTableRow = rateTable.select("tr").get(i);
-			String curr = currTableRow.select("td").get(0).text();
-			String cleanedCurr = curr.replace("\u00a0","");
-			String enumInput  = "Indian Rupee";	//This will the currency selected from enum
-			if(cleanedCurr.equals(enumInput)){
-				targetCurr = Double.parseDouble(currTableRow.select("td").get(1).text());
-				equivalentsgd = Double.parseDouble(currTableRow.select("td").get(2).text());
-				//System.out.println("For " + targetCurr + " rupees you get " + equivalentsgd + " S$.");
-				toSGD = equivalentsgd/targetCurr;
-				toINR = (1.0)/toSGD;
-				
-				rates.setConversion(Currency.getInstance("SGD"), Currency.getInstance("INR"), toINR);
-				rates.setConversion(Currency.getInstance("INR"), Currency.getInstance("SGD"), 1/toINR);
-				return;
-			}
+	private void extractRates(Element table) {
+		int length = table.select("tr").size();
+		for (int i=0; i<length; i++) {
+			Currency toCur = getCurrency(table, i);
+			int unit = getUnit(table, i);
+			double rate = getRate(table, i);
+			double perRate = (unit > 0) ? (rate/unit) : rate;
+			
+			rates.addAvailableCurrency(toCur);
+			rates.setConversion(base, toCur, perRate);
+			
+			log.info(toCur.getDisplayName() + " : " + unit + " units @ " + rate);
 		}
 	}
-
-	/*
-	 * TODO: Need to crawl page and add all currencies.
-	 * Currently hard-coded it.
-	 */
-	private void updateListOfCurrencies() {
-		rates.addAvailableCurrency(Currency.getInstance("INR"));
-		rates.addAvailableCurrency(Currency.getInstance("SGD"));
+	
+	private double getRate(Element table, int index) {
+		double rate = Double.parseDouble(table.select("tr").get(index).select("td").get(2).text());
+		return rate;
 	}
 
+	private int getUnit(Element table, int index) {
+		int unit = Integer.parseInt(table.select("tr").get(index).select("td").get(1).text());
+		return unit;
+	}
+
+	private Currency getCurrency(Element table, int index) {
+		String currencyName  = table.select("tr").get(index).select("td").get(0).text().replace("\u00a0", "");
+		return CurrencyAdapter.getInstance(currencyName);
+	}
 }
---
+++ src/main/java/io/xrates/rateprovider/util/CurrencyAdapter.java
@@ -0,0 +1,41 @@
+package io.xrates.rateprovider.util;
+
+import java.util.Currency;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class CurrencyAdapter {
+	private static Map<String, Currency> map = null;
+	
+	public static Currency getInstance(String currencyName) {
+		if (map == null) {
+			map = new HashMap<String, Currency>();
+			Set<Currency> availableCurrencies = Currency.getAvailableCurrencies();
+			for (Currency cur : availableCurrencies) {
+				map.put(cur.getDisplayName(), cur);
+			}
+			
+			//Adding exceptions
+			addExceptions();
+		}
+		
+		if (map.containsKey(currencyName)) {
+			return map.get(currencyName);
+		} else {
+			return null;
+		}
+	}
+
+	private static void addExceptions() {
+		map.put("Sterling Pound", Currency.getInstance("GBP"));
+		map.put("Danish Kroner", Currency.getInstance("DKK"));
+		map.put("Norwegian Kroner", Currency.getInstance("NOK"));
+		map.put("Swedish Kroner", Currency.getInstance("SEK"));
+		map.put("Chinese Renminbi(Offshore)", Currency.getInstance("CNY"));
+		map.put("Chinese Renminbi", Currency.getInstance("CNY"));
+		map.put("Korean Won", Currency.getInstance("KRW"));
+		map.put("Sri Lanka Rupee", Currency.getInstance("LKR"));
+		map.put("Saudi Rial", Currency.getInstance("SAR"));
+	}
+}
