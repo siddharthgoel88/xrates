d40613ffd75e7c167a7f11e73dbde48c90cb7a70
Shantanu Alshi <shantanuals@gmail.com>
2016-07-03 16:20:40 +0800

    Add Email Verification Token Bean

---
+++ src/main/java/io/xrates/backend/datamodel/beans/VerificationToken.java
@@ -0,0 +1,53 @@
+package io.xrates.backend.datamodel.beans;
+
+import javax.persistence.*;
+import java.util.Date;
+
+
+public class VerificationToken {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String token;
+
+    private Date expiryDate;
+
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Date getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(Date expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+
+}
